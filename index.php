6f0f0e5fa2a4fa74dc283684d1dd57e694e2b6a9
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-02-03 21:58:02 -0300

    funfa pls

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $url = "https://chat.miauuapi.com/$end";
 $ch = curl_init(); // INICIA CONEXÃO
 curl_setopt($ch, CURLOPT_URL, $url); // LIGAÇÃO COM O LINK
 
-header('Access-Control-Allow-Origin: https://miauuchat.herokuapp.com');
-$hr = array("authorization: giwXuRY4ucOqQvz2g08OhMy89KxxZrv0", 'Access-Control-Allow-Origin: https://miauuchat.herokuapp.com');
+header('Access-Control-Allow-Origin: *');
+$hr = array("authorization: giwXuRY4ucOqQvz2g08OhMy89KxxZrv0", 'Access-Control-Allow-Origin: *');
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // HABILITA RESPONSE
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // DESABILITAR CERTIFICAÇÃO SSL
