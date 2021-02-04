a145a0e01f700f996aed485685049930cb62b04c
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-02-03 21:40:52 -0300

    funfa pls

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ $url = "https://chat.miauuapi.com/$end";
 $ch = curl_init(); // INICIA CONEXÃO
 curl_setopt($ch, CURLOPT_URL, $url); // LIGAÇÃO COM O LINK
 
-$hr = array("authorization: giwXuRY4ucOqQvz2g08OhMy89KxxZrv0", 'Access-Control-Allow-Origin: *', 'Access-Control-Allow-Headers: X-Requested-With', 'content-type', 'access-control-allow-origin', 'access-control-allow-methods', 'access-control-allow-headers');
+header('Access-Control-Allow-Origin: *');
+$hr = array("authorization: giwXuRY4ucOqQvz2g08OhMy89KxxZrv0", 'Access-Control-Allow-Origin: *');
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // HABILITA RESPONSE
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1); // DESABILITAR CERTIFICAÇÃO SSL
